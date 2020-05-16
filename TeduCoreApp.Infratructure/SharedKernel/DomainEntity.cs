deeab67670e3a55face30e48f95800fe37034d38
Hồ Hữu Nhân <nhan111321@gmail.com>
2020-05-16 22:01:52 +0700

    Lession 11 - Triển khai tạo các Entity để sinh ra cơ sở dữ liệu

---
+++ TeduCoreApp.Infratructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace TeduCoreApp.Infrastructure.SharedKernel
+{
+    public class DomainEntity<T>
+    {
+        public T Id { get; set; }
+
+        ///
+        /// True if domain entity has an identity
+        ///
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+    }
+}
